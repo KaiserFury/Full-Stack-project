e999a9b45376c29f79e3d95aae76c3e15efbc3f3
Abhishek Ranjan <abhisek.214099@gmail.com>
2026-07-14 19:58:10 +0530

    Add Listing model, DB init script, and main  frontend template

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
+// Import required packages
 const express = require("express");
-const app  = express();
+const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const Listing = require("./models/listing.js");
 
-
+// Set up view engine and views folder for EJS templates
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded(path.join(__dirname, "views")));
+
+// Connect to MongoDB when the app starts
+main()
+  .then(() => {
+    console.log("Connection is successful with DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/WanderLust");
+}
+
+// Home route
+app.get("/", (req, res) => {
+  res.send("Working");
+});
+
+app.get("/listings", async(req, res) => {
+  const allListing = await Listing.find();
+  res.render("listings/index.ejs", {allListing});
+});
+
+// Start the server on port 8080
+app.listen(8080, () => {
+  console.log("Server is running");
+});
---
+++ models/listing.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    type: String,
+    default: "https://media.istockphoto.com/id/1442179368/photo/maldives-island.jpg?s=612x612&w=0&k=20&c=t38FJQ6YhyyZGN91A8tpn3nz9Aqcy_aXolImsOXOZ34=",
+    set: (v) =>
+      v === ""
+        ? "https://media.istockphoto.com/id/1442179368/photo/maldives-island.jpg?s=612x612&w=0&k=20&c=t38FJQ6YhyyZGN91A8tpn3nz9Aqcy_aXolImsOXOZ34="
+        : v,
+  },
+  price: {
+    type: Number,
+  },
+  location: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
